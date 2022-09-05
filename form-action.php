15acdfb7e607c5b628c936d0f17c3a96b0086b65
akshitsingh1234 <77962446+akshitsingh1234@users.noreply.github.com>
2022-09-05 17:47:09 +0530

    Add files via upload

--- form-action.php
+++ form-action.php
@@ -1,5 +1,12 @@
 <?php
 
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+
         $first_name =  $_REQUEST['firstname'];
         $last_name = $_REQUEST['lastname'];
         $email = $_REQUEST['email'];
@@ -36,10 +43,39 @@ if(mysqli_query($conn, $sql)){
                 . "$gender\n $address\n $email");
 }
 
+$query="SELECT * FROM message";
+$query_run=mysqli_query($conn, $query);
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'First Name');
+$sheet->setCellValue('B1', 'Last Name');
+$sheet->setCellValue('C1', 'Email');
+$sheet->setCellValue('D1', 'Phone Number');
+
+$row_count=2;
+foreach($query_run as $data)
+{
+$sheet->setCellValue('A'. $row_count, $data['firstname']);
+$sheet->setCellValue('B'. $row_count, $data['lastname']);
+$sheet->setCellValue('C'. $row_count, $data['email']);
+$sheet->setCellValue('D'. $row_count, $data['number']);
+$row_count++;
+}
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('JatayuDroneTraining.xlsx');
+
+
+
 mysqli_close($conn);
 header("Location: randomtp.php");
+
+
 }
 else{
     echo "Enter Phone Number with correct format <br>";
     }
+
+
+
 ?>
