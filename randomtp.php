d1b6fc76950d37e671fdd70d37ad01634d3437e4
akshitsingh1234 <77962446+akshitsingh1234@users.noreply.github.com>
2022-09-11 20:48:50 +0530

    Used subject field instead of number

--- randomtp.php
+++ randomtp.php
@@ -5,33 +5,40 @@
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 <link rel="stylesheet" type="text/css" href="anythingtp.css">
 <link href="randomtp.html" >
 </head>
+<style>
+::placeholder {
+  font-family:"Arial";
+  opacity: 1; 
+}
 
+::textarea:focus{
+  background: #ddd;
+}
+
+
+</style>
 <body>
-
 <div id="logo">
 <img src="Website_Pics/Drone.gif" width="1520" height="800">
   <div class="Heading"><h1>Jatayu Drone Academy</h1></div>
 <button class="Button">GET STARTED</button>
+<button class="Button5" type="button" onclick="window.open('https://forms.gle/dkY6MeZWpHDPs59o9')">APPLY</button>
   <div class="Button1"><u>ABOUT</u></div>
   <div class="Button2"><u>CLASS</u></div>
   <div class="Button3"><u>CONTACT</u></div>
-  <div class="Button4"><u>LOGIN</u></div>
+  <div class="Button4"><u>ADMIN_LOGIN</u></div>
 <br><br><br><br>
 <hr style="height:2px;border-width:0;color:black;background-color:black">
 <br><br><br>
 <levels><b>Level 1</b>-  Ground Training</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
   <p style="color:yellow">Classification & Categorization of Drones and Type Certification of Drones</p>
@@ -50,16 +57,12 @@
     <img src="Website_Pics/Capture4.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <br><br><br>
 <levels><b>Level 2</b>-  Simulator Flying</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
     <p style="color:yellow">Train drone pilots to safety and efficiently operate according to FAA guidelines.</p><br>
@@ -78,16 +81,12 @@
     <img src="Website_Pics/Capture8.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <br><br><br>
 <levels><b>Level 3</b>-  One on one practical flying</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
 <p style="color:yellow">Register with the authorities as a learner and for the theory exams.</p><br>
@@ -106,16 +105,12 @@
     <img src="Website_Pics/Capture12.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <br><br><br>
 <levels><b>Level 4</b>-  Solo flying</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
 <p style="color:yellow">Identify & select different types of Drones and illustrate Fundamentals of Flight
@@ -137,44 +132,36 @@ Chargers, Connectors etc. in drone flying.</p><br>
     <img src="Website_Pics/Capture16.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <br><br><br>
-<levels><b>Level 5</b>-  Instrument flying</levels>
+<levels><b>Level 5</b>-  Ground Control Station(GCS)</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
-<p style="color:yellow">Airspeed indicator gives the aeroplane's speed directly and, indirectly, pitch attitude (airspeed increasing – nose low, airspeed decreasing – nose high).</p><br>
+<p style="color:yellow">A ground control station is typically a software application, running on a ground-based computer, that communicates with your UAV via wireless telemetry.</p><br>
     <img src="Website_Pics/Capture17.png" alt="Snow" style="width:80%" data-aos="fade-up">
   </div>
   <div class="column">
-<p style="color:yellow">Altimeter directly indicates the height of the aeroplane above a datum, usually sea level. Indirectly, it indicates pitch attitude (altitude decreasing – nose low, altitude increasing – nose high).</p>
+<p style="color:yellow">Displaying real-time data on the UAVs performance and position and can serve as a “virtual cockpit”, showing many of the same instruments that you would have if you were flying a real plane.</p>
     <img src="Website_Pics/Capture18.png" alt="Forest" style="width:80%" data-aos="fade-up">
   </div>
   <div class="column">
-<p style="color:yellow">Heading indicator directly indicates the aeroplane's heading when aligned with the magnetic compass.</p><br><br>
+<p style="color:yellow">Controlling a UAV in flight, uploading new mission commands and setting parameters.</p><br><br>
     <img src="Website_Pics/Capture19.png" alt="Mountains" style="width:80%" data-aos="fade-up">
   </div>
 <div class="column">
-<p style="color:yellow">The turn coordinator (TC), or turn indicator, directly indicates the rate of change of direction.</p><br><br>
+<p style="color:yellow">Monitor the live video streams from a UAV’s cameras.</p><br><br><br>
     <img src="Website_Pics/Capture20.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <br><br><br>
 <levels><b>Level 6</b>-  Exposure to various emergency procedure</levels>
 <br><br><br>
-
-
 <br>
 <br>
 <br>
-
 <div class="row">
   <div class="column">
   <p style="color:yellow">Loss of Drone Control - Drone not responding to pilot's control or the aircraft is operating independently from the pilot's control.</p><br>
@@ -193,59 +180,45 @@ Chargers, Connectors etc. in drone flying.</p><br>
     <img src="Website_Pics/Capture24.png" alt="Mountainsi" style="width:80%" data-aos="fade-up">
   </div>
 </div>
-
 <div id="myForm">
   <form action="form-action.php" method="post"  id="form-container">
-
     <h1 style="color:yellow">Login</h1>
-
     <label for="firstname" style="color:yellow"><b>First Name</b></label>
     <input type="text" placeholder="Enter First Name" name="firstname" required>
-
 <label for="lastname" style="color:yellow"><b>Last Name</b></label>
     <input type="text" placeholder="Enter Last Name" name="lastname" required>
-
  <label for="email" style="color:yellow"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" required>  
  
-<label for="number" style="color:yellow"><b>Phone Number</b></label>
-    <input type="text" placeholder="Enter Number" name="number" required>
-
+<label for="number" style="color:yellow"><b>Subject</b></label><br><br>
+    <textarea name="subject" type="text" placeholder="Enter Subject"  rows="14" cols="35" wrap="soft" maxlength="2000" style="overflow:hidden; resize: none; background: #f1f1f1;
+  opacity:1.0" autofocus required></textarea>
     <button type="submit" class="btn">Submit</button>
     <button type="button" class="btn cancel" onclick="closeForm()">Close</button> 
 </form>
 </div>
 </div>
-
-
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
-
       AOS.init({
         duration: 3000,
         once: true,
       });
 </script>
-
 <script>
   const btn = document.querySelector("Button");
-
 btn.addEventListener("click", openloginbox);
-
 const btn1=document.querySelector("btn");
   btn1.addEventListener("click", submit);
-
 function openloginbox()
 {
   document.getElementById("myForm").style.display = "block";
   
 }
-
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 </script>
-
         
         </body>
         </html>
