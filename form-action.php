48d890b78e08c415e285b43e6cd8489d68211883
akshitsingh1234 <77962446+akshitsingh1234@users.noreply.github.com>
2022-09-18 21:25:28 +0530

    Place for internal operation of query submission

--- form-action.php
+++ form-action.php
@@ -2,6 +2,7 @@
 
 
 require 'vendor/autoload.php';
+require 'randomtp.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
@@ -25,65 +26,25 @@ $conn = mysqli_connect(hostname: $host,
 if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_error());
 }           
+if(!empty($first_name) && !empty($last_name) && !empty($email) && !empty($subject) )  
+
 $sql = "INSERT INTO message VALUES ('$first_name',
             '$last_name','$email','$subject')";
 
 if(mysqli_query($conn, $sql)){
-            echo "<h3>data stored in a database successfully."
-                . " Please browse your localhost php my admin"
-                . " to view the updated data</h3>";
- 
-            echo nl2br("\n$first_name\n $last_name\n "
-                . "$gender\n $address\n $email");
-}
+  if(!empty($first_name) && !empty($last_name) && !empty($email) && !empty($subject) )  
+     
+echo "<script type='text/javascript'>alert('Query submitted successfully');</script>";
 
-$query="SELECT * FROM message";
-$query_run=mysqli_query($conn, $query);
-$spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'First Name');
-$sheet->setCellValue('B1', 'Last Name');
-$sheet->setCellValue('C1', 'Email');
-$sheet->setCellValue('D1', 'Subject');
-
-$row_count=2;
-foreach($query_run as $data)
-{
-$sheet->setCellValue('A'. $row_count, $data['firstname']);
-$sheet->setCellValue('B'. $row_count, $data['lastname']);
-$sheet->setCellValue('C'. $row_count, $data['email']);
-$sheet->setCellValue('D'. $row_count, $data['subject']);
-$row_count++;
 
 }
-$spreadsheet->getActiveSheet()->getStyle('A1:E999')
-    ->getAlignment()->setWrapText(true); 
-
-$spreadsheet->getActiveSheet()->getStyle("A1:D1")->getFont()->setBold( true );
-$spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(20);
-$spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(20);
-$spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(30);
-$spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(30);
-
-$sheet->getStyle('A')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-$sheet->getStyle('B')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-$sheet->getStyle('C')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-$sheet->getStyle('D')->getAlignment()->setVertical(\PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_TOP);
-
-$sheet->getStyle('A')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-$sheet->getStyle('B')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-$sheet->getStyle('C')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-$sheet->getStyle('D')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
-
-
-
-$writer = new Xlsx($spreadsheet);
-$writer->save('JatayuDroneTraining.xlsx');
 
 
 
 mysqli_close($conn);
 header("Location: randomtp.php");
+
+
 
 
 
